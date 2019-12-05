54b9592b370aa8693a0d308b9c493ee75fa34df4
Rasmus Porsager <rasmus@porsager.com>
2019-12-05 08:54:38 +0100

    Include info about the package needed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # `npm-needs-install`
 
-This small utility runs through your package.json, and then checks if any modules are missing or needs to be updated in `node_modules` by checking their package.json version. This is faster than running `npm install` directly so you can run it everytime at startup and always be up to date if you just pulled new changes or switched branches.
+This small utility runs through your package.json, and tells you about the fist module which is missing or needs to be updated in `node_modules` by checking their package.json version. This is faster than running `npm install` directly so you can run it everytime at startup and always be up to date if you just pulled new changes or switched branches.
--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const fs = require('fs')
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
 
 module.exports = function() {
-  return [].concat(Object.entries(pkg.dependencies || {}))
+  const result = {}
+  ;[].concat(Object.entries(pkg.dependencies || {}))
   .concat(Object.entries(pkg.devDependencies || {}))
-  .some(([k, v, i]) =>
-    !fs.existsSync(path.join('node_modules', k)) ||
-    (v.match(/[^/]/) && JSON.parse(fs.readFileSync(path.join('node_modules', k, 'package.json'), 'utf8')).version !== v)
-  )
+  .some(([k, v, i]) => {
+    const pkg = fs.existsSync(path.join('node_modules', k)) && JSON.parse(fs.readFileSync(path.join('node_modules', k, 'package.json'), 'utf8'))
+    if (!pkg || ((v.match(/[^/]/) && pkg.version !== v)))
+      return Object.assign(result, { name: k, needed: v, installed: pkg ? pkg.version : 'not found' })
+  })
+  return result
 }
